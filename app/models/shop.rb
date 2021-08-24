8980ae1cd8a01ae92c879a89aabbc3ef742e529a
davidhaffner <david.haffner06@gmail.com>
2021-08-24 15:03:36 +0200

    push push quel dis clarisse

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,8 +1,10 @@
 class Shop < ApplicationRecord
+  has_one_attached :photo
+  
   has_many :cashbacks
   validates :name, presence: true
   validates :address, presence: true
-  validates :active, presence: true, default: false # actif après validation du format de ticket
+  validates :active, default: false # actif après validation du format de ticket
   # validates :latitude, presence: true
   # validates :longitude, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,47 @@
+require "open-uri"
 p "Clean de la DBz"
 Shop.destroy_all
 p "Creation de quelques magazins"
-Shop.create(name: "Il Tores", siret: 28485795642152, address: "48 Avenue de la Plaine Bossue 06000 Nice")
-Shop.create(name: "LE ZEN", siret: 28485795642152, address: "48 Avenue de la Plaine Bossue 06000 Nice")
-Shop.create(name: "Proxima", siret: 28485795642152, address: "48 Avenue de la Plaine Bossue 06000 Nice")
-Shop.create(name: "Del centrores", siret: 28485795642152, address: "48 Avenue de la Plaine Bossue 06000 Nice")
-Shop.create(name: "La margaritas", siret: 28485795642152, address: "48 Avenue de la Plaine Bossue 06000 Nice")
-Shop.create(name: "Le tulipe", siret: 28485795642152, address: "48 Avenue de la Plaine Bossue 06000 Nice")
-Shop.create(name: "La platzia", siret: 28485795642152, address: "48 Avenue de la Plaine Bossue 06000 Nice")
-Shop.create(name: "Le tabouret cent pieds", siret: 28485795642152, address: "48 Avenue de la Plaine Bossue 06000 Nice")
-Shop.create(name: "La clairière de derrière", siret: 28485795642152, address: "48 Avenue de la Plaine Bossue 06000 Nice")
-Shop.create(name: "Shopping", siret: 28485795642152, address: "48 Avenue de la Plaine Bossue 06000 Nice")
-Shop.create(name: "Le coin du cercle", siret: 28485795642152, address: "48 Avenue de la Plaine Bossue 06000 Nice")
-Shop.create(name: "La plage pavé", siret: 28485795642152, address: "48 Avenue de la Plaine Bossue 06000 Nice")
-Shop.create(name: "Le lac salé", siret: 28485795642152, address: "48 Avenue de la Plaine Bossue 06000 Nice")
+file = URI.open('https://res.cloudinary.com/dcmbxjvgh/image/asset/v1629807748/shoptest1-f306e228af58d6f14a166e2044d3f9d9.jpg')
+shop = Shop.new(name: "Il Tores", siret: 2147483647, address: "48 Avenue de la Plaine Bossue 06000 Nice")
+shop.photo.attach(io: file, filename: 'shoptest1.jpg', content_type: 'image/jpg')
+shop.save!
+file = URI.open('https://res.cloudinary.com/dcmbxjvgh/image/asset/v1629807750/shoptest2-01cae586bfe10359f70c4829cf24ec81.jpg')
+shop = Shop.new(name: "LE ZEN", siret: 2147483647, address: "48 Avenue de la Plaine Bossue 06000 Nice")
+shop.save!
+file = URI.open('https://res.cloudinary.com/dcmbxjvgh/image/asset/v1629807751/shoptest3-9ec4a7cdfe3398fdd4db5e8a3b9446cd.jpg')
+shop = Shop.new(name: "Proxima", siret: 2147483647, address: "48 Avenue de la Plaine Bossue 06000 Nice")
+shop.save!
+file = URI.open('https://res.cloudinary.com/dcmbxjvgh/image/asset/v1629807748/shoptest1-f306e228af58d6f14a166e2044d3f9d9.jpg')
+shop = Shop.new(name: "Del centrores", siret: 2147483647, address: "48 Avenue de la Plaine Bossue 06000 Nice")
+shop.save!
+file = URI.open('https://res.cloudinary.com/dcmbxjvgh/image/asset/v1629807748/shoptest1-f306e228af58d6f14a166e2044d3f9d9.jpg')
+shop = Shop.new(name: "La margaritas", siret: 2147483647, address: "48 Avenue de la Plaine Bossue 06000 Nice")
+shop.save!
+file = URI.open('https://res.cloudinary.com/dcmbxjvgh/image/asset/v1629807750/shoptest2-01cae586bfe10359f70c4829cf24ec81.jpg')
+shop = Shop.new(name: "Le tulipe", siret: 2147483647, address: "48 Avenue de la Plaine Bossue 06000 Nice")
+shop.save!
+file = URI.open('https://res.cloudinary.com/dcmbxjvgh/image/asset/v1629807751/shoptest3-9ec4a7cdfe3398fdd4db5e8a3b9446cd.jpg')
+shop = Shop.new(name: "La platzia", siret: 2147483647, address: "48 Avenue de la Plaine Bossue 06000 Nice")
+shop.save!
+file = URI.open('https://res.cloudinary.com/dcmbxjvgh/image/asset/v1629807748/shoptest1-f306e228af58d6f14a166e2044d3f9d9.jpg')
+shop = Shop.new(name: "Le tabouret cent pieds", siret: 2147483647, address: "48 Avenue de la Plaine Bossue 06000 Nice")
+shop.save!
+file = URI.open('http://auvergne-centrefrance.com/hotels/haute-loire/moudeyres/photos/pre-bossu-max.jpg')
+shop = Shop.new(name: "La clairière de derrière", siret: 2147483647, address: "48 Avenue de la Plaine Bossue 06000 Nice")
+shop.save!
+file = URI.open('https://res.cloudinary.com/dcmbxjvgh/image/asset/v1629807750/shoptest2-01cae586bfe10359f70c4829cf24ec81.jpg')
+shop = Shop.new(name: "Shopping", siret: 2147483647, address: "48 Avenue de la Plaine Bossue 06000 Nice")
+shop.save!
+file = URI.open('https://res.cloudinary.com/dcmbxjvgh/image/asset/v1629807751/shoptest3-9ec4a7cdfe3398fdd4db5e8a3b9446cd.jpg')
+shop = Shop.new(name: "Le coin du cercle", siret: 2147483647, address: "48 Avenue de la Plaine Bossue 06000 Nice")
+shop.save!
+file = URI.open('https://res.cloudinary.com/dcmbxjvgh/image/asset/v1629807748/shoptest1-f306e228af58d6f14a166e2044d3f9d9.jpg')
+shop = Shop.new(name: "La plage pavé", siret: 2147483647, address: "48 Avenue de la Plaine Bossue 06000 Nice")
+shop.save!
+file = URI.open('https://res.cloudinary.com/dcmbxjvgh/image/asset/v1629807750/shoptest2-01cae586bfe10359f70c4829cf24ec81.jpg')
+shop = Shop.new(name: "Le lac salé", siret: 2147483647, address: "48 Avenue de la Plaine Bossue 06000 Nice")
+shop.save!
 
 p "Création des utilisateurs"
 user1 = User.create(email:"david@ducobu.com", password:"123456")
