042b4451f9f341f552bdcde33e606fba3df5bc64
davidhaffner <david.haffner06@gmail.com>
2021-08-25 17:27:35 +0200

    add linkto logout and route dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'dashboards#home'
   resources :caschbacks, only: %i[new create]
-  resources :dashboards, only: %i[show home]
+  resource :dashboard, only: %i[show home]
   devise_for :users
 end
