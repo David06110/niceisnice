5e53de0f04522151f1559525e63ddac940d73af2
davidhaffner <david.haffner06@gmail.com>
2021-08-26 17:44:17 +0200

    upload ok / conversion json a faire

--- app/controllers/cashbacks_controller.rb
+++ app/controllers/cashbacks_controller.rb
@@ -1,7 +1,33 @@
 class CashbacksController < ApplicationController
+  require 'uri'
+  require 'net/http'
+  require 'net/https'
+  require 'mime/types'
   def new
+    @new_cashback = Cashback.new(user: current_user)
+
+
+    
+
   end
 
   def create
+    url = URI("https://api.mindee.net/v1/products/mindee/expense_receipts/v3/predict")
+    @file = params[:cashback][:ticket]
+    
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    request = Net::HTTP::Post.new(url)
+    request["Authorization"] = 'Token 06cde4e0a0b3a6c856bbe7a6383021ce'
+    request.set_form([['document', File.open(@file)]], 'multipart/form-data')
+    
+    response = http.request(request)
+    @testcela = JSON.parse(response.read_body)
+    
+
+
+
+
+    redirect_to '/dashboard'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'dashboards#home'
-  resources :caschbacks, only: %i[new create]
+  resources :cashbacks, only: %i[new create]
   resource :dashboard, only: %i[show home]
   devise_for :users
 end
