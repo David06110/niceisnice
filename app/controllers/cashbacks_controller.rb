9c1e12bd57869f0bb585419c7f29cb764508b81f
davidhaffner <david.haffner06@gmail.com>
2021-08-27 14:54:40 +0200

    debug scan ticket

--- app/controllers/cashbacks_controller.rb
+++ app/controllers/cashbacks_controller.rb
@@ -11,25 +11,31 @@ class CashbacksController < ApplicationController
     @new_cashback = Cashback.new(user: current_user)
 
     url = URI("https://api.mindee.net/v1/products/mindee/expense_receipts/v3/predict")
-    @file = params[:cashback][:ticket]
+    if params[:cashback].present?
+      @file = params[:cashback][:ticket]
+      
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+      request = Net::HTTP::Post.new(url)
+      request["Authorization"] = 'Token 06cde4e0a0b3a6c856bbe7a6383021ce'
+      request.set_form([['document', File.open(@file)]], 'multipart/form-data')
+      
+      response = http.request(request)
+      @testcela = JSON.parse(response.body, symbolize_names: true)
+      
+      @tk_current = @testcela[:document][:inference][:prediction][:total_incl][:value]
     
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    request = Net::HTTP::Post.new(url)
-    request["Authorization"] = 'Token 06cde4e0a0b3a6c856bbe7a6383021ce'
-    request.set_form([['document', File.open(@file)]], 'multipart/form-data')
-    
-    response = http.request(request)
-    @testcela = JSON.parse(response.body, symbolize_names: true)
-    
-    @tk_current = @testcela[:document][:inference][:prediction][:total_incl][:value]
-  
-    @new_cashback.amount = (@tk_current * 5) / 100
-    @new_cashback.shop = Shop.first
-    @new_cashback.save!
-    
-    sleep(0.5)
+      @new_cashback.amount = (@tk_current * 5) / 100
+      @new_cashback.shop = Shop.first
 
-    redirect_to '/dashboard'
+      if @new_cashback.save!
+        sleep(0.5)
+        redirect_to '/dashboard'
+      else
+        render :new
+      end
+    else
+      render :new
+    end
   end
 end
