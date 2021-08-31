622a51630fa0cb117209b67f976426349752f74f
Clarisse Cosatti <clarisse.cosatti@gmail.com>
2021-08-31 18:56:19 +0200

    frontdashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get 'coupons/show'
 
-  resources :shops , only: %i[index]
+  resources :shops , only: %i[index show]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'dashboards#home'
