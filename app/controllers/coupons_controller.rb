de14e7894030bdfcd353783d00b1d6833b9ecaac
AudreyBerger <berger.audrey06@gmail.com>
2021-08-31 13:07:45 +0200

    cashback_button

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,4 @@
+class CouponsController < ApplicationController
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'coupons/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'dashboards#home'
   resources :cashbacks, only: %i[new create index]
   resource :dashboard, only: %i[show home]
   devise_for :users
+  resource :coupons, only: %i[show]
 end
