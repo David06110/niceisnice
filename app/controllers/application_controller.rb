65ea7d1d49cbb38c7b0fae680f7aba5eb8c48360
davidhaffner <david.haffner06@gmail.com>
2021-09-01 10:26:20 +0200

    replace link for ticket scan button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :init_scan
+
 
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
+  private
+
+  def init_scan
+    @new_cashback = Cashback.new
   end
 end
